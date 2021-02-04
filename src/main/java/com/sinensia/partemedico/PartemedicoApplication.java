535df59f4c445ff0b2646bd4288c633653cefc63
Jorge Nunez <jorgedam95@gmail.com>
2021-02-04 12:49:38 +0100

    creacion del proyecto sinensia

---
+++ src/main/java/com/sinensia/partemedico/PartemedicoApplication.java
@@ -0,0 +1,13 @@
+package com.sinensia.partemedico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PartemedicoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PartemedicoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sinensia/partemedico/PartemedicoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sinensia.partemedico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PartemedicoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
