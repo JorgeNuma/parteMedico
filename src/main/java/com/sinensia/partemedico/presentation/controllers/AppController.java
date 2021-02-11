5a7a84f36c04db5d43e165ef3be88fb2f63ade9c
Pablo Santos <pablosantosexposito@gmail.com>
2021-02-11 16:32:19 +0100

    Cambios estéticos/ Corrección de errores

--- src/main/java/com/sinensia/partemedico/presentation/controllers/AppController.java
+++ src/main/java/com/sinensia/partemedico/presentation/controllers/AppController.java
@@ -9,9 +9,11 @@ import javax.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.sinensia.partemedico.business.model.Reporte;
 import com.sinensia.partemedico.business.model.Sexo;
@@ -20,6 +22,7 @@ import com.sinensia.partemedico.business.services.ReporteService;
 import com.sinensia.partemedico.business.services.UsuarioService;
 
 @Controller
+@CrossOrigin
 @RequestMapping("/partemedico")
 public class AppController {
 
